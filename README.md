3deefd9824fe4f2f020aa0a974f5c37f9df7c312
jeffchee <jeffrey.chee@yahoo.com>
2019-05-28 13:41:39 -0700

    reorganizing the frontend/ adding new html page to have better display for user

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Career-Services-Coding-Challenge---Tip-Calculator
 Just the tip!
+
+
+additional help w3 school
---
+++ public/tip.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+
+
+var userInput = $(".userInput").val().trim();
+var tipRange = $("#tipRange").val().trim();
+var tipOutput = $("#tipOutput")
+
+var splitInputBetween = $("#splitInputBetween");
+
+tipOutput.innerHTML = tipRange;
+
+
+
+$("#tipOutput").onclick = function myFunction() {
+
+    tipOutput.text(this.value)
+}
+
+
+$("#splitOutputBetween").text(splitOutputBetween[0].value)
+
+
+
+console.log(userInput)
+console.log(tipRange[0].value)
+console.log(tipOutput.innerHTML)
+
+console.log(userInput)
+console.log(tipRange)
+console.log(tipOutput)
+
+// pseudo
+// get all the inputs
+// get the multipling and dividing of numbers down. the process
+
+// ** i want to make it live so when you move the numbers move too 
+
+// validation
+// if input of number is entered, app already ready to use with preset numbers
+// preset 10% /// 2 people
+
+// toggle1 is selected, it needs to wait for number to be inputted first before finishing
+// reminder that something is missing
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
