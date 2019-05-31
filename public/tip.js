79cfb7e410293b28e1c68bcb24eceac05cfe37d9
jeffchee <jeffrey.chee@yahoo.com>
2019-05-31 07:49:22 -0700

    redoing layout/ starting the use of firebase to store data online

--- public/tip.js
+++ public/tip.js
@@ -1,46 +1,73 @@
-$(document).ready(function() {
-
-
-var userInput = $(".userInput").val().trim();
-var tipRange = $("#tipRange").val().trim();
-var tipOutput = $("#tipOutput")
-
-var splitInputBetween = $("#splitInputBetween");
-
-tipOutput.innerHTML = tipRange;
+$(document).ready(function () {
 
 
 
-$("#tipOutput").onclick = function myFunction() {
 
-    tipOutput.text(this.value)
-}
+    var userInput = document.getElementById("userInput");
+
+    var tipRange = document.getElementById("tipRange");
+    var tipOutput = document.getElementById("tipOutput");
+    var splitInput = document.getElementById("splitInput");
+    var splitOutput = document.getElementById("splitOutput");
+
+    var tipRangeValue = tipRange.value
+    var splitInputValue = splitInput.value
+    var userInputValue = userInput.value
+
+    var split = "";
+    var range = "";
+
+    console.log(tipRangeValue);
+    console.log(splitInputValue);
+    console.log(userInputValue);
+
+    tipOutput.innerHTML = tipRangeValue;
+    splitOutput.innerHTML = splitInputValue;
+
+    tipRange.oninput = function () {
+        tipOutput.innerHTML = this.value;
+        this.range = tipOutput.innerHTML;
+        range = this.range
+        console.log("tip percentage is :" + range);
+
+    }
+
+    splitInput.oninput = function () {
+        splitOutput.innerHTML = this.value;
+        this.split = splitOutput.innerHTML;
+        split = this.split
+        console.log("split between " + split + " people");
+        return split;
+    }
 
 
-$("#splitOutputBetween").text(splitOutputBetween[0].value)
+
+    $("#addTip").on("click", function () {
 
 
+        console.log(splitInput);
 
-console.log(userInput)
-console.log(tipRange[0].value)
-console.log(tipOutput.innerHTML)
+        
+    })
 
-console.log(userInput)
-console.log(tipRange)
-console.log(tipOutput)
+    // 100 x % = X
+    // X+100 = Y
+    // Y/# = each 
+    // if then statements to determine equal amounts or 
 
-// pseudo
-// get all the inputs
-// get the multipling and dividing of numbers down. the process
 
-// ** i want to make it live so when you move the numbers move too 
+    // pseudo
+    // get all the inputs
+    // get the multipling and dividing of numbers down. the process
 
-// validation
-// if input of number is entered, app already ready to use with preset numbers
-// preset 10% /// 2 people
+    // ** i want to make it live so when you move the numbers move too 
 
-// toggle1 is selected, it needs to wait for number to be inputted first before finishing
-// reminder that something is missing
+    // validation
+    // if input of number is entered, app already ready to use with preset numbers
+    // preset 10% /// 2 people
+
+    // toggle1 is selected, it needs to wait for number to be inputted first before finishing
+    // reminder that something is missing
 
 
 
